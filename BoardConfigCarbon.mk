ae3866118c2d1b8540795956ed4f068e80198df2
Simon Sickle <simon@simonsickle.com>
2016-02-20 17:54:57 +0100

    Initalize carbon makefiles
    Signed-off-by: Simon Sickle <simon@simonsickle.com>

---
+++ BoardConfigCarbon.mk
@@ -0,0 +1,23 @@
+# Kernel settings
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
+TARGET_KERNEL_SOURCE := kernel/htc/flounder
+TARGET_KERNEL_CONFIG := flounder_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
+
+# AudioFX
+TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
+TARGET_IGNORE_VENDOR_AUDIO_EFFECTS_CONF := true
+
+# TWRP
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
+# Enable vendor image symlink
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
